806f6ec3447cec4f7774529bc8eef44154a03ffd
Tony Garnock-Jones <tonyg@leastfixedpoint.com>
2025-05-27 17:52:48 +0200

    Use requirements.txt

--- scripts/setup_env.sh
+++ scripts/setup_env.sh
@@ -1,7 +1,4 @@
 conda create -n thesis
 conda init thesis
 conda activate thesis
-pip3 install numpy
-pip3 install syndicate-py
-pip3 install torch
-pip3 install tensorflow
+pip3 install -r ../requirements.txt
